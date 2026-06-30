01e9707769e40c1e7aa4da433b6bd7f5b16f375f
panggilsajarey <150323912+raynard05@users.noreply.github.com>
2026-06-30 10:31:24 +0700

    this is opnly test3

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,6 +1,6 @@
 // app/cp_page/page.tsx
 import React from 'react';
-import './cp.css';
+
 
 // Diubah menjadi ProfilPage (P kapital)
 export default function InfoPage() {
--- app/latar/page.tsx
+++ app/latar/page.tsx
@@ -1,6 +1,5 @@
 // app/cp_page/page.tsx
 import React from 'react';
-import './cp.css';
 
 // Diubah menjadi ProfilPage (P kapital)
 export default function LatarPage() {
